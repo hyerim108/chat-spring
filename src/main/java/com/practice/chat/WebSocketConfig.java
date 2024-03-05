566ae1c0b5af2d973a9b69b25b2270ff8c0840a5
hyerim108 <batlove108@naver.com>
2024-03-05 13:54:25 +0900

    feat : socket handler & config 작성

---
+++ src/main/java/com/practice/chat/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.practice.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * handler를 이용하여 websocket을 활성화하기위한 config작성
+ * @EnbleWebSocket을 작성하여 활성화
+ * endpoint작성 /ws/chat
+ * CORS : setAllowedOrigins("*")작성
+ */
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}
---
+++ src/main/java/com/practice/chat/handler/WebSocketChatHandler.java
@@ -0,0 +1,40 @@
+package com.practice.chat.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.chat.DTO.ChatMessage;
+import com.practice.chat.DTO.ChatRoom;
+import com.practice.chat.Service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+/**
+ * socket 통신은 서버와 클라이언트가 1:n으로 관계를 맺는다. 한서버에 여러 클라이언트 접속가능
+ * 서버에는 여러 클라이언트가 발송한 메세지를 받아 처리해줄 handler가 필요하다
+ * textwebsocketHandler를 상속받아 handler작성
+ * client로 받은 메세지를 출력하고 환영메세지를 보내줌
+ **/
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+    //클라이언트가 작성한 메세지를 받아서 처리해줄 핸들러
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        //받은 메세지를 console log에 payload에 담아 출력
+        String payload = message.getPayload();
+        System.out.println("payload{} "+payload);
+        log.info("payload {}", payload);
+        //환영메세지를 보내줌
+//        TextMessage textMessage = new TextMessage("Welcome Chat Server");
+//        session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+        chatRoom.handleActions(session,chatMessage,chatService);
+    }
+}
